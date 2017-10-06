1cc12683dc3f24c6ff1f289076793215ef134f67
Kenza <kenza.dieye@gmail.com>
2017-10-06 10:49:35 +0200

    Recuperating-api-activities

---
+++ untitled.rb
@@ -0,0 +1,6 @@
+require 'yelp'
+
+client = Yelp::Client.new({ consumer_key: '7HhphBL10gBsaQKoO8nhbw',
+                            consumer_secret: 'EmRZH2Rl3Hf55ggfzAKqRajO1KEyOSuTN4E1kPkbZZeZnCUX6PtOQOiXuUnfybYv',
+                            token: 'dIw8bTDMd7CtJi0G8rEeP4pHH_juYgzH-umXRhgoRZGTLnjN0R_zjAZRd6PH-4YC7047mxY_c7SmQ6cNyUEK_dc44Ik7ARR3TQGmWz8oRD77rULBmISeuxb6CD7WWXYx'
+                          })
