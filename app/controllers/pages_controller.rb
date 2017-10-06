419babb39574bef064a9f47e7b619c9d1331aefe
Kenza <kenza.dieye@gmail.com>
2017-10-06 17:52:34 +0200

    getting activities related to places selected by the user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
+require_relative "../services/fetch_activities_service"
+require_relative "../services/defining_activities_categories_service"
+
 class PagesController < ApplicationController
   def home
   end
@@ -7,6 +10,15 @@ class PagesController < ApplicationController
   end
 
   def setactivities
+    @businesses = []
     @places = params['trip']['places']
+    @places.each do |place|
+      CATEGORIES.each do |category|
+        category.each do |elem|
+          results = search(elem, place)
+          @businesses << results['businesses']
+        end
+       end
+     end
   end
 end
--- app/services/defining_activities_categories_service.rb
+++ app/services/defining_activities_categories_service.rb
@@ -1,9 +1,10 @@
-ACTIVE_LIFE = "Active Life, Bicycles"
-ARTS_ENTERTAINMENT = "Arts, Entertainment, Mass Media, Education, Religious Organizations"
-BEAUTY_RELAXATION = "Beauty & Spas "
-SHOPPING = "Shopping"
-HOTELS_TRAVEL = "Hotels, Travel"
-FOOD_RESTAURANT = "Food, Restaurants"
-NIGHTLIFE = "Nightlife"
+ACTIVE_LIFE = ["Active Life", "Bicycles"]
+ARTS_ENTERTAINMENT = ["Arts", "Entertainment", "Mass Media", "Education", "Religious Organizations"]
+BEAUTY_RELAXATION = ["Beauty & Spas "]
+SHOPPING = ["Shopping"]
+HOTELS_TRAVEL = ["Hotels", "Travel"]
+FOOD_RESTAURANT = ["Food", "Restaurants"]
+NIGHTLIFE = ["Nightlife"]
 
-CATEGORIES = [ACTIVE_LIFE, ARTS_ENTERTAINMENT, BEAUTY_RELAXATION, SHOPPING, HOTELS_TRAVEL, FOOD_RESTAURANT, NIGHTLIFE]
+CATEGORIES = [FOOD_RESTAURANT, ACTIVE_LIFE, BEAUTY_RELAXATION, SHOPPING, HOTELS_TRAVEL, NIGHTLIFE, ARTS_ENTERTAINMENT,]
+
