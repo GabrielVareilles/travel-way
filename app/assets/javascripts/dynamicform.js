fb47512e8d0b072042202bc68054ae6f4f5d9b4c
Aurelia Amalvict <aurelia.amalvict@gmail.com>
2017-10-10 18:04:04 +0200

    change file

--- app/assets/javascripts/dynamicform.js
+++ app/assets/javascripts/dynamicform.js
@@ -10,7 +10,3 @@ button.addEventListener("click", (event) => {
   form.insertAdjacentHTML('beforeend', `<div class='form-group text optional trip_places'><label class='control-label text optional', for='trip_places'>City ${city_counter}</label><input class='form-control string optional', placeholder='Where?', type='text', name='trip[places][]', id='trip_'></div>`
    );
 })
-
-
-
-// "<%= f.input :places, as: :array, placeholder: 'Where do you want to go next?', label: 'City #{city_counter}' %>"
