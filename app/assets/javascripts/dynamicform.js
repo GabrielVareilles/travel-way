368a8a2b29cb4885c83fcbcae62f17adcaf1ad0d
Aurelia Amalvict <aurelia.amalvict@gmail.com>
2017-10-10 18:19:16 +0200

    changes

--- app/assets/javascripts/dynamicform.js
+++ app/assets/javascripts/dynamicform.js
@@ -1,13 +1,14 @@
 $(document).on('turbolinks:load', function() {
-  let city_counter = 1;
+  var city_counter = 1;
 
-  const button = document.getElementById("button");
+  var button = document.getElementById("button");
 
   button.addEventListener("click", (event) => {
-    console.log(button);
-    const form = document.getElementById('forminputs');
-    console.log(form);
-    city_counter += 1;
+    // console.log(button);
+    var form = document.getElementById('forminputs');
+    // console.log(form);
+    city_counter = city_counter + 1;
+    // console.log(`${city_counter + 1}`);
     form.insertAdjacentHTML('beforeend', `<div class='form-group text optional trip_places'><label class='control-label text optional', for='trip_places'>City ${city_counter}</label><input class='form-control string optional', placeholder='Where?', type='text', name='trip[places][]', id='trip_'></div>`
      );
   })
