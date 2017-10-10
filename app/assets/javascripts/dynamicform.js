59e3e00237da6c858f1f06341552dce98c1c1baa
Aurelia Amalvict <aurelia.amalvict@gmail.com>
2017-10-10 18:22:25 +0200

    changes in dynamicform_js

--- app/assets/javascripts/dynamicform.js
+++ app/assets/javascripts/dynamicform.js
@@ -9,8 +9,8 @@ $(document).on('turbolinks:load', function() {
     // console.log(form);
     city_counter = city_counter + 1;
     // console.log(`${city_counter + 1}`);
-    form.insertAdjacentHTML('beforeend', `<div class='form-group text optional trip_places'><label class='control-label text optional', for='trip_places'>City ${city_counter}</label><input class='form-control string optional', placeholder='Where?', type='text', name='trip[places][]', id='trip_'></div>`
-     );
+    var template = `<div class='form-group text optional trip_places'><label class='control-label text optional', for='trip_places'>City ${city_counter}</label><input class='form-control string optional', placeholder='Where?', type='text', name='trip[places][]', id='trip_'></div>`
+    form.insertAdjacentHTML('beforeend', template);
   })
 })
 
