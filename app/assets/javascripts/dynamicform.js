e5fa5f257f89207cf9d1de4204e3410b4c29debc
Aurelia Amalvict <aurelia.amalvict@gmail.com>
2017-10-10 18:27:25 +0200

    changes es6 in jQuery

--- app/assets/javascripts/dynamicform.js
+++ app/assets/javascripts/dynamicform.js
@@ -3,13 +3,13 @@ $(document).on('turbolinks:load', function() {
 
   var button = document.getElementById("button");
 
-  button.addEventListener("click", (event) => {
+  button.on("click", function() {
     // console.log(button);
     var form = document.getElementById('forminputs');
     // console.log(form);
     city_counter = city_counter + 1;
     // console.log(`${city_counter + 1}`);
-    var template = `<div class='form-group text optional trip_places'><label class='control-label text optional', for='trip_places'>City ${city_counter}</label><input class='form-control string optional', placeholder='Where?', type='text', name='trip[places][]', id='trip_'></div>`
+    var template = "<div class='form-group text optional trip_places'><label class='control-label text optional', for='trip_places'>City " + city_counter + "</label><input class='form-control string optional', placeholder='Where?', type='text', name='trip[places][]', id='trip_'></div>"
     form.insertAdjacentHTML('beforeend', template);
   })
 })
