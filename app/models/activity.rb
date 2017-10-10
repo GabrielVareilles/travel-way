e9a1e27f50c3058e8861576d0ebe5459f2ed548c
Kenza <kenza.dieye@gmail.com>
2017-10-10 14:55:35 +0200

    maj-seeds

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,11 +1,5 @@
 class Activity < ApplicationRecord
-
   belongs_to :trip
   geocoded_by :display_address
   after_validation :geocode, if: :display_address_changed?
-
-  has_and_belongs_to_many :projects
-  # geocoded_by :display_address
-  # after_validation :geocode, if: :display_address_changed?
-
 end
