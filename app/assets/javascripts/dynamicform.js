7cb6840721c74c48a2b79af1b2f67b6a48d5cc57
Charlie Boucher <charlie.boucher@edhec.com>
2017-10-11 11:20:29 +0200

    Nieme correction du javascript

--- app/assets/javascripts/dynamicform.js
+++ app/assets/javascripts/dynamicform.js
@@ -1,16 +1,15 @@
 $(document).on('turbolinks:load', function() {
   var city_counter = 1;
 
-  var button = document.getElementById("button");
+  var button = $("#button");
 
   button.on("click", function() {
     // console.log(button);
-    var form = document.getElementById('forminputs');
+    var form = $('#forminputs');
     // console.log(form);
     city_counter = city_counter + 1;
     // console.log(`${city_counter + 1}`);
     var template = "<div class='form-group text optional trip_places'><label class='control-label text optional', for='trip_places'>City " + city_counter + "</label><input class='form-control string optional', placeholder='Where?', type='text', name='trip[places][]', id='trip_'></div>"
-    form.insertAdjacentHTML('beforeend', template);
-  })
-})
-
+    form.append(template);
+  });
+});
