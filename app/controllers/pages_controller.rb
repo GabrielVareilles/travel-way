e15b5ad46939434b556dd7b0d9317a71d30e4e3f
Yann IRBAH <yann.irbah@gmail.com>
2017-10-12 20:23:11 +0200

    Implements ajax loading for activities

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,51 +15,45 @@ class PagesController < ApplicationController
   end
 
   def setactivities
-    @places = []
     @trip = Trip.new(trip_params)
-    @params = params
-    params['trip']['places'].reject{ |c| c.empty? }.each do |place|
-      @places << { name: place, activities: [] }
+
+    @places = params['trip']['places'].reject{ |p| p.empty? }.map do |place|
+      { name: place, activities: [] }
     end
 
-    Activity.bulk_insert(ignore: true) do |worker|
-    @places.each do |place|
-      ActivitiesCategories::CATEGORIES.each do |category|
-        category.each do |subcategory|
-          results = FetchActivitiesService.new(subcategory, place[:name]).()
-          #results.map! { |activity| activity.to_h }
-          results.map! do |activity|
-            activity['category'] = category[0]
-            activity['yelp_id'] = activity['id']
-            activity.delete('id')
-            activity['latitude'] = activity['coordinates']['latitude']
-            activity['longitude'] = activity['coordinates']['longitude']
-            activity.delete('coordinates')
-            activity['place_name'] = activity['location']['city']
-            activity['display_address'] = activity['location']['display_address'].join(' ')
-            activity.delete('location')
-            activity
-          end
-
-          place[:activities].map! { |activity| activity.is_a?(Hash) ? activity : activity.attributes }
-          place[:activities] << results.flatten
-          place[:activities].flatten!
-        end
-
-        yelp_ids = []
-
-          place[:activities].each do |activity|
-            worker.add(activity)
-            yelp_ids << activity['yelp_id']
-          end
-
-        place[:activities] = Activity.where(yelp_id: yelp_ids).to_a
-      end
-
-      @categories = place[:activities].map { |h| h['category'] }.uniq
-    end
-    end
-    @trip = Trip.new
+    # places.each do |place|
+    #   ActivitiesCategories::CATEGORIES.flatten.each do |category|
+    #       results = FetchActivitiesService.new(category, place[:name]).()
+    #       results.map! do |activity|
+    #         activity['category'] = category
+    #         activity['yelp_id'] = activity['id']
+    #         activity.delete('id')
+    #         activity['latitude'] = activity['coordinates']['latitude']
+    #         activity['longitude'] = activity['coordinates']['longitude']
+    #         activity.delete('coordinates')
+    #         activity['place_name'] = activity['location']['city']
+    #         activity['display_address'] = activity['location']['display_address'].join(' ')
+    #         activity.delete('location')
+    #         activity
+    #       end
+    #
+    #       place[:activities] += results
+    #   end
+    # end
+    #
+    # yelp_ids = []
+    # Activity.bulk_insert(ignore: true) do |worker|
+    #   @categories = places.each_with_object([]) do |place, o|
+    #     o << place[:activities].map do |activity|
+    #       worker.add(activity)
+    #       yelp_ids << activity['yelp_id']
+    #       activity['category']
+    #     end
+    #   end.first.uniq
+    # end
+    #
+    # @places = Activity.where(yelp_id: yelp_ids).group_by { |a| a['place_name'] }
+    # @trip = Trip.new
   end
 
   # Charlie: Nouvelle methode à appeler dans la boucle de la méthode précédente
---
+++ app/javascript/packs/activities_timeline.js
@@ -0,0 +1,60 @@
+function cardTemplate (activity) {
+  return `
+    <div class="col-md-4 col-sm-6 portfolio panel all ${activity.category} animated fadeIn">
+      <div class="card-setactivity">
+        <div class="panel-body">
+          <div class="portfolio-title">
+            <div class="portfolio-icon">
+              <div><input type="checkbox" name="trip[activity_ids][] value="${activity.id}"></div>
+            </div>
+            <div class="title">
+              <h4>${activity.name}</h4>
+              <em>Homepage redesign for a well known brand.</em>
+            </div>
+          </div>
+        </div>
+        <div class="activities-portfolio-image" style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${activity.image_url}');">
+        </div>
+      </div>
+      <a class="" data-toggle="modal" data-target="#info-${activity.id}">View details</a>
+    </div>
+  `
+}
+
+function buttonTemplate(city, category) {
+  return `
+    <button type="button" class="allFilter btn background-black filter-selected animated fadeInDown" onclick="filterCategories('${city}', '${category}')">${category}</button>
+  `
+}
+
+function fetchActivitiesForPlace (city, callback) {
+  const timeline = document.getElementById('timeline');
+  const list = timeline.querySelector(`#${city} .activities-list`);
+  const categoriesList = timeline.querySelector(`#${city} .categories-list .portfolio-filters`);
+
+  fetch(`/search?place=${city}`, { credentials: "same-origin" })
+  .then(response => response.json())
+  .then(results => {
+    list.innerHTML = '';
+    results.activities.forEach((activity, index) => {
+      setTimeout(() => {
+        list.insertAdjacentHTML('beforeend', cardTemplate(activity));
+      }, 150 * index)
+    });
+
+    results.categories.forEach((category, index) => {
+      setTimeout(() => {
+        categoriesList.insertAdjacentHTML('beforeend', buttonTemplate(city, category));
+      }, 150 * index)
+    });
+  })
+}
+
+$(() => {
+  const timeline = document.getElementById('timeline');
+  if (timeline) {
+    JSON.parse(timeline.dataset.places).forEach((place) => {
+      fetchActivitiesForPlace(place);
+    })
+  }
+})
