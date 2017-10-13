a228cca4d89f0a9bb64261872ea2efc0ebd908bd
Adrien du Peloux <adrien.dupeloux@edhec.com>
2017-10-14 01:01:40 +0200

    WIP

--- app/javascript/packs/activities_timeline.js
+++ app/javascript/packs/activities_timeline.js
@@ -1,79 +1,95 @@
 
 function cardTemplate (activity) {
   return `
-    <div class="col-md-4 col-sm-6 portfolio panel all ${activity.category} animated fadeIn">
-      <div class="card-setactivity">
-        <div class="panel-body">
-          <div class="portfolio-title">
-            <div class="portfolio-icon">
-              <div><input type="checkbox" name="trip[activity_ids][]" value="${activity.id}"></div>
-            </div>
-            <div class="title">
-              <h4>${activity.name}</h4>
-              <em>Homepage redesign for a well known brand.</em>
-            </div>
+  <div class="col-md-4 col-sm-6 portfolio panel all ${activity.category} animated fadeIn">
+    <div class="card-setactivity">
+      <div class="panel-body">
+        <div class="portfolio-title">
+          <div class="portfolio-icon">
+            <div><input type="checkbox" name="trip[activity_ids][]" value="${activity.id}"></div>
+          </div>
+          <div class="title">
+            <h4>${activity.name}</h4>
+            <em>Homepage redesign for a well known brand.</em>
           </div>
         </div>
-        <div class="activities-portfolio-image" style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${activity.image_url}');">
-        </div>
       </div>
-      <div data-yelp-id="${activity.yelp_id}">
-        <a class="details-link" data-toggle="modal" data-target="#info-${activity.yelp_id}">View details</a>
+      <div class="activities-portfolio-image" style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${activity.image_url}');">
       </div>
     </div>
+    <div data-yelp-id="${activity.yelp_id}">
+      <a class="details-link" data-toggle="modal" data-target="#info-${activity.yelp_id}">View details</a>
+    </div>
+  </div>
 
- <!--  Modal -->
-    <div class="modal fade" id="info-${activity.yelp_id}" tabindex="-1" role="dialog" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-         <button type="button" class="modal-close-btn" data-dismiss="modal" aria-label="Close"><span class="glyphicon glyphicon-remove"></span></button>
-         <div class="modal-body container-fluid portfolio mobile">
-            <!--  Carousel -->
-            <div id="carousel-portfolio-2" class="carousel slide" data-ride="carousel">
-              <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                  <img src="${activity.image_url}" alt="" />
-                </div>
+  <!--  Modal -->
+  <div class="modal fade" id="info-${activity.yelp_id}" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <button type="button" class="modal-close-btn" data-dismiss="modal" aria-label="Close"><span class="glyphicon glyphicon-remove"></span></button>
+        <div class="modal-body container-fluid portfolio mobile">
+          <!--  Carousel -->
+          <div id="carousel-portfolio-2" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                <img src="${activity.image_url}" alt="" />
               </div>
             </div>
-            <div class="row">
-              <div class="col-md-5">
+          </div>
+          <div class="row">
+            <div class="col-md-5">
+              <div class="infos">
                 <div class="portfolio-title">
                   <div class="portfolio-icon">
-                    <div>M</div>
+                    <div>${activity.category.charAt(0).toUpperCase()}</div>
                   </div>
                   <div class="title">
                     <h4>${activity.name}</h4>
-                    <em>UI / UX design.</em>
+                    <em>${activity.category}</em>
                   </div>
                 </div>
+                <div class="info-list">
+                  <ul>
+                    <li>${activity.display_phone}</li>
+                    <li>${activity.display_address}</li>
+                  </ul>
+                </div>
               </div>
-              <div class="modal-text col-md-7">
-                <p><ul id="${ activity.id }-reviews"></ul></p>
-              </div>
+            </div>
+            <div class="modal-text col-md-7">
+              <p><table id="${ activity.id }-reviews"></table></p>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
+
+
 
   `
 }
 
 function buttonTemplate(city, category) {
   return `
-    <button type="button" class="allFilter btn background-black filter-selected animated fadeInDown" onclick="filterCategories('${city}', '${category}')">${category}</button>
+  <button type="button" class="allFilter btn background-black filter-selected animated fadeInDown" onclick="filterCategories('${city}', '${category}')">${category}</button>
   `
 }
 
 function fetchReviews(yelp_id) {
+  const reviewList = document.querySelector(`#info-${yelp_id} table`)
   fetch(`/fetchreviews?yelp_id=${yelp_id}`, { credentials: "same-origin" })
-    .then(response => response.json())
-    .then(results => {
-      console.log(results);
-      document.querySelector(`#info-${yelp_id} ul`).insertAdjacentHTML("beforeend", `<li>${results['reviews'][0]['text']}</li>`)
-    });
+  .then(response => response.json())
+  .then(results => {
+    if (reviewList.innerHTML === "") {
+      results.reviews.forEach((review) => {
+      reviewList.insertAdjacentHTML("beforeend", `<tr class="review"><td><div class="image-cropper"><img src="${review.user.image_url}"></div></td><td class="review-text"><b>${review.user.name}:</b> ${review.text}</td></tr>`);
+      });
+    };
+  });
 };
+
+
 
 function fetchActivitiesForPlace (city, callback) {
   const timeline = document.getElementById('timeline');
@@ -84,13 +100,14 @@ function fetchActivitiesForPlace (city, callback) {
   .then(response => response.json())
   .then(results => {
     list.innerHTML = '';
+    let chosenActivities = 0;
     results.activities.forEach((activity, index) => {
       setTimeout(() => {
         list.insertAdjacentHTML('beforeend', cardTemplate(activity));
         let link = document.querySelectorAll(`#${city} .details-link`)[index]
         link.addEventListener("click", (event) => {
-            const yelp_id = event.target.parentNode.dataset.yelpId;
-            fetchReviews(yelp_id);
+          const yelp_id = event.target.parentNode.dataset.yelpId;
+          fetchReviews(yelp_id);
         });
       }, 150 * index);
     });
@@ -98,10 +115,23 @@ function fetchActivitiesForPlace (city, callback) {
     results.categories.forEach((category, index) => {
       setTimeout(() => {
         categoriesList.insertAdjacentHTML('beforeend', buttonTemplate(city, category));
+        document.querySelectorAll(".card-setactivity").forEach( (card) => {
+          card.addEventListener("click", (event) => {
+            if (card.classList.contains("selected-card")) {
+              chosenActivities -= 1;
+              card.classList.remove("selected-card");
+              card.querySelector("input").checked = false;
+            } else {
+              chosenActivities += 1;
+              card.classList.add("selected-card");
+              card.querySelector("input").checked = true;
+            }
+          });
+        });
       }, 150 * index)
     });
-  })
-}
+  });
+};
 
 $(() => {
   const timeline = document.getElementById('timeline');
