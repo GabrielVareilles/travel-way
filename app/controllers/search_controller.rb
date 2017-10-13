519272e2a1e5a279876fe762abe138798d8e74de
Adrien du Peloux <adrien.dupeloux@edhec.com>
2017-10-13 12:48:18 +0200

    ok we get reviews

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -20,6 +20,7 @@ class SearchController < ApplicationController
           activity['place_name'] = activity['location']['city']
           activity['display_address'] = activity['location']['display_address'].join(' ')
           activity.delete('location')
+          activity['reviews'] = findreviews(activity)
           worker.add(activity)
           h_activities << activity
         end
@@ -29,6 +30,19 @@ class SearchController < ApplicationController
     activities = Activity.where(yelp_id: h_activities.map { |r| r['yelp_id'] }).to_a
 
     render json: {categories: activities.map(&:category).uniq, activities: activities}
+  end
+
+  private
+   def findreviews(activity)
+    reviews = []
+    apireviews = FetchReviewsService.new(activity["yelp_id"]).()["reviews"]
+    apireviews.each do |review|
+      reviews << {
+        name: review["user"]["name"],
+        reviewtext: review["text"]
+      }
+    end
+    reviews.to_s
   end
 
 end
